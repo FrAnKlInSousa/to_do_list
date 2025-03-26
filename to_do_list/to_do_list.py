2af851d65824dd232d496b2fe55434106b272cf3
franklin <frank.fkn.23@gmail.com>
2025-03-26 00:05:01 -0300

    adicionando endpoints e atualizando pyproject

--- to_do_list/to_do_list.py
+++ to_do_list/to_do_list.py
@@ -1,24 +1,76 @@
-from fastapi import FastAPI, status
+from http import HTTPStatus
+
+from fastapi import FastAPI, HTTPException, status
 from fastapi.responses import HTMLResponse
 
-from to_do_list.schemas import Message
+from to_do_list.schemas import (
+    Message,
+    UserDB,
+    UserList,
+    UserPublic,
+    UserSchema,
+)
 
 app = FastAPI()
 
+database = []
 
-@app.get('/', status_code=status.HTTP_200_OK, response_model=Message)
+
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
-    return {'message': 'Olá Mundo!'}
+    return {'message': 'Olá, mundo!'}
 
 
 @app.get('/hello', status_code=status.HTTP_200_OK, response_class=HTMLResponse)
 def hello():
     return """
-<html>
-<head>
-<title>Olá mundo!</title>
-</head>
-<body>
-<h1> Olá Mundo </h1>
-</body>
-</html>"""
+    <html>
+        <head>
+        <title>Nosso olá mundo!</title>
+        </head>
+        <body>
+        <h1> Olá Mundo! </h1>
+        </body>
+        </html>"""
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(id=len(database) + 1, **user.model_dump())
+    database.append(user_with_id)
+    return user_with_id
+
+
+@app.get('/users/', response_model=UserList)
+def read_users():
+    return {'users': database}
+
+
+@app.put('/users/{user_id}', response_model=UserPublic)
+def update_user(user_id: int, user: UserSchema):
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+    user_with_id = UserDB(id=user_id, **user.model_dump())
+    database[user_id - 1] = user_with_id
+    return user_with_id
+
+
+@app.delete('/user/{user_id}', response_model=Message)
+def delete_user(user_id: int):
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+
+    del database[user_id - 1]
+    return {'message': 'User deleted'}
+
+
+@app.get('/users/{user_id}', response_model=UserPublic)
+def read_user(user_id: int):
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(HTTPStatus.NOT_FOUND, detail='User not found')
+    user_with_id = database[user_id - 1]
+    return user_with_id
