2d4b655fcccfa455815a98ef468caade59d7d847
franklin <frank.fkn.23@gmail.com>
2025-03-31 12:23:49 -0300

    movendo rota de token

---
+++ to_do_list/routers/auth.py
@@ -0,0 +1,30 @@
+from http import HTTPStatus
+
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from to_do_list.database import get_session
+from to_do_list.models import User
+from to_do_list.schemas import Token
+from to_do_list.security import (
+    create_access_token,
+    verify_password,
+)
+
+router = APIRouter(prefix='/auth', tags=['auth'])
+
+
+@router.post('/token', response_model=Token)
+def login_for_access_token(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    session: Session = Depends(get_session),
+):
+    user = session.scalar(select(User).where(User.email == form_data.username))
+    if not user or not verify_password(form_data.password, user.password):
+        raise HTTPException(
+            HTTPStatus.BAD_REQUEST, detail='Incorrect email or password'
+        )
+    access_token = create_access_token(data={'sub': user.email})
+    return {'access_token': access_token, 'token_type': 'Bearer'}
--- to_do_list/to_do_list.py
+++ to_do_list/to_do_list.py
@@ -1,20 +1,14 @@
 from http import HTTPStatus
 
-from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi import FastAPI, status
 from fastapi.responses import HTMLResponse
-from fastapi.security import OAuth2PasswordRequestForm
-from sqlalchemy import select
-from sqlalchemy.orm import Session
 
-from to_do_list.database import get_session
-from to_do_list.models import User
-from to_do_list.schemas import Message, Token
-from to_do_list.security import (
-    create_access_token,
-    verify_password,
-)
+from to_do_list.schemas import Message
+from to_do_list.routers import users, auth
 
 app = FastAPI()
+app.include_router(users.router)
+app.include_router(auth.router)
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
@@ -33,17 +27,3 @@ def hello():
         <h1> Olá Mundo! </h1>
         </body>
         </html>"""
-
-
-@app.post('/token', response_model=Token)
-def login_for_access_token(
-    form_data: OAuth2PasswordRequestForm = Depends(),
-    session: Session = Depends(get_session),
-):
-    user = session.scalar(select(User).where(User.email == form_data.username))
-    if not user or not verify_password(form_data.password, user.password):
-        raise HTTPException(
-            HTTPStatus.BAD_REQUEST, detail='Incorrect email or password'
-        )
-    access_token = create_access_token(data={'sub': user.email})
-    return {'access_token': access_token, 'token_type': 'Bearer'}
