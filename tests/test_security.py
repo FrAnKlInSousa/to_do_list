22aa0a04bca4f923bbc063c484cf177a84412856
franklin <frank.fkn.23@gmail.com>
2025-03-31 11:46:17 -0300

    adicionando teste test_security

--- tests/test_security.py
+++ tests/test_security.py
@@ -1,12 +1,49 @@
+from http import HTTPStatus
+
 from jwt import decode
 
-from to_do_list.security import ALGORITHM, SECRET_KEY, create_access_token
+from to_do_list.security import (
+    ALGORITHM,
+    SECRET_KEY,
+    create_access_token,
+)
 
 
 def test_jwt():
     data = {'sub': 'test@test.com'}
-    token = create_access_token(data)
+    token_jwt = create_access_token(data)
 
-    result = decode(token, SECRET_KEY, [ALGORITHM])
+    result = decode(token_jwt, SECRET_KEY, [ALGORITHM])
     assert result['sub'] == data['sub']
     assert result['exp']
+
+
+def test_jwt_invalid_token(client):
+    response = client.delete(
+        '/users/1', headers={'Authorization': 'Bearer token-invalido'}
+    )
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {'detail': 'Could not validate credentials'}
+
+
+def test_get_current_user_username_invalid(session, client, user):
+    data = {'sub': 'wrong_email@test.com'}
+    token_jwt = create_access_token(data)
+    response = client.delete(
+        'users/1', headers={'Authorization': f'Bearer {token_jwt}'}
+    )
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {'detail': 'Could not validate credentials'}
+
+
+def test_get_current_user_without_sub(session, client, user):
+    data = {}
+    token_jwt = create_access_token(data)
+    response = client.delete(
+        'users/1', headers={'Authorization': f'Bearer {token_jwt}'}
+    )
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {'detail': 'Could not validate credentials'}
