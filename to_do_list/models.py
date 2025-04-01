8a1c13df936c62664d3f76c227870bae649e5b2c
franklin <frank.fkn.23@gmail.com>
2025-04-01 10:06:09 -0300

    criando todo_state enum

--- to_do_list/models.py
+++ to_do_list/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from enum import Enum
 
 from sqlalchemy import func
 from sqlalchemy.orm import Mapped, mapped_column, registry
@@ -19,3 +20,11 @@ class User:
     updated_at: Mapped[datetime] = mapped_column(
         init=False, server_default=func.now(), server_onupdate=func.now()
     )
+
+
+class TodoState(str, Enum):
+    draft = 'draft'
+    todo = 'todo'
+    doing = 'doing'
+    done = 'done'
+    trash = 'trash'
