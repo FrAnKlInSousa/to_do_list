81e0f6e595f58daeec61d547a3ddf2a439c89833
franklin <frank.fkn.23@gmail.com>
2025-04-02 13:04:38 -0300

    adicionando rota de update patch

--- to_do_list/routers/todos.py
+++ to_do_list/routers/todos.py
@@ -7,7 +7,13 @@ from sqlalchemy.orm import Session
 
 from to_do_list.database import get_session
 from to_do_list.models import Todo, TodoState, User
-from to_do_list.schemas import Message, TodoList, TodoPublic, TodoSchema
+from to_do_list.schemas import (
+    Message,
+    TodoList,
+    TodoPublic,
+    TodoSchema,
+    TodoUpdate,
+)
 from to_do_list.security import get_current_user
 
 router = APIRouter(prefix='/todos', tags=['todos'])
@@ -65,3 +71,24 @@ def delete_todo(todo_id: int, session: T_Session, user: CurrentUser):
     session.delete(todo)
     session.commit()
     return {'message': 'Task has been deleted successfully.'}
+
+
+@router.patch('/{todo_id}', response_model=TodoPublic)
+def patch_todo(
+    todo_id, session: T_Session, user: CurrentUser, todo: TodoUpdate
+):
+    db_todo = session.scalar(
+        select(Todo).where(Todo.user_id == user.id, Todo.id == todo_id)
+    )
+    if not db_todo:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='Task not found.'
+        )
+
+    for key, value in todo.model_dump(exclude_unset=True).items():
+        setattr(db_todo, key, value)
+    session.add(db_todo)
+    session.commit()
+    session.refresh(db_todo)
+
+    return db_todo
